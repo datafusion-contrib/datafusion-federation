c083f27bf5feef1dfc415218be26ade2cc7bb099
Michiel De Backker <mail@backkem.me>
2024-02-13 15:37:12 +0100

    Add SessionStateProvider

---
+++ sources/flight-sql/src/server/session.rs
@@ -0,0 +1,26 @@
+use datafusion::{error::Result, execution::context::SessionState};
+use tonic::Request;
+
+// SessionStateProvider is a trait used to provide a SessionState for a given
+// request.
+pub trait SessionStateProvider: Sync + Send {
+    fn new_context(&self, request: &Request<()>) -> Result<SessionState>;
+}
+
+// StaticSessionStateProvider is a simple implementation of SessionStateProvider that
+// uses a static SessionState.
+pub(crate) struct StaticSessionStateProvider {
+    state: SessionState,
+}
+
+impl StaticSessionStateProvider {
+    pub fn new(state: SessionState) -> Self {
+        Self { state }
+    }
+}
+
+impl SessionStateProvider for StaticSessionStateProvider {
+    fn new_context(&self, _request: &Request<()>) -> Result<SessionState> {
+        Ok(self.state.clone())
+    }
+}
