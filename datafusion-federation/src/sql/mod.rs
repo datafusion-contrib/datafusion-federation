b7020ea1d88ef8885e9ee39a8bc0da85f693dc0a
hozan23 <hozan23@karyontech.net>
2024-08-23 11:27:17 +0200

    override supports_rewrite for FederationOptimizerRule and SQLFederationOptimizerRule

--- datafusion-federation/src/sql/mod.rs
+++ datafusion-federation/src/sql/mod.rs
@@ -93,6 +93,12 @@ impl OptimizerRule for SQLFederationOptimizerRule {
     fn name(&self) -> &str {
         "federate_sql"
     }
+
+    /// XXX
+    /// Does this rule support rewriting owned plans (rather than by reference)?
+    fn supports_rewrite(&self) -> bool {
+        false
+    }
 }
 struct SQLFederationPlanner {
     executor: Arc<dyn SQLExecutor>,
